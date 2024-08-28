2b5295f4d478f60566ff25d4a4851ef6f57c0c56
Meghna Singh <138083187+MeghnaSingh15@users.noreply.github.com>
2024-08-28 22:22:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,19 @@
 function mincost(arr)
-{ 
-//write your code here
+{
+  let totalCost = 0;
+  arr.sort((a, b) => a - b);
+  while (arr.length > 1) {
+    let first = arr.shift();
+    let second = arr.shift(); 
+    let cost = first + second;
+    totalCost += cost;
+    arr.push(cost);
+    arr.sort((a, b) => a - b);
+  }
+
+  return totalCost;
+}
+
 // return the min cost
   
 }
